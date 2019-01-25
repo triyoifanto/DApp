57bb3a0baa9b8aeaa6f9f158c7b7983e236a0049
triyoifanto <ipandto@gmail.com>
2019-01-25 14:54:15 +0700

    - add 3rd part @auth0/angular-jwt to secure token on the client - use the angular-jwt to check if the token exxpired or not - use the angular-jwt to decode the token - get the user name value from decoded token to be displayed on the page

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -28,8 +28,8 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token; // retun true or false value, based on token value
+    // use auth0 jwt service
+    return this.authService.loggedIn(); // retun true or false value, based on token value
   }
 
   logout() {
