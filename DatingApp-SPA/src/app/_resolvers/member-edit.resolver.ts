4ce4b1c9e325e18b991aec6264a370250f41fe09
triyoifanto <ipandto@gmail.com>
2019-01-29 18:26:46 +0700

    - add member edit component

---
+++ DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
+
+@Injectable()
+export class MemberEditResolver implements Resolve<User> {
+    constructor(private userService: UserService, private router: Router,
+        private alertify: AlertifyService, private authService: AuthService) {}
+
+        // if use resolve we don't need to use subscrib. it will be automatic subscrib
+    resolve(route: ActivatedRouteSnapshot): Observable<User> {
+        return this.userService.getUser(this.authService.decodedToken.nameid).pipe(
+            catchError(error => {
+                this.alertify.error('problem retrieving your data');
+                this.router.navigate(['/members']);
+                return of(null);
+            })
+        );
+    }
+}
